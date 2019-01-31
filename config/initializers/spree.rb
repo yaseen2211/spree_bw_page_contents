6408282993cfa22106b7de639075fbfecee124e2
yaseen iqbal <p136011@nu.edu.pk>
2019-01-31 17:23:54 +0500

    make globalize

---
+++ config/initializers/spree.rb
@@ -0,0 +1,2 @@
+SpreeI18n::Config.available_locales = [:en, :es, :'pt-BR'] # displayed on frontend select box
+SpreeGlobalize::Config.supported_locales = [:en, :es, :'pt-BR'] # displayed on translation forms
